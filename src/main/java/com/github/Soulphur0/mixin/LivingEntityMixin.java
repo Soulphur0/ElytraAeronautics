729f56779baad65f57a55fa8fa241a6fb7d80c3a
Soulphur <ismaelbe2002@gmail.com>
2022-10-30 00:58:14 +0200

    Fixed speed calculations, now additional speed is clamped, simplified code and found out fields to nullify in order to negate downwards speed.

--- src/main/java/com/github/Soulphur0/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/Soulphur0/mixin/LivingEntityMixin.java
@@ -70,12 +70,10 @@ public abstract class LivingEntityMixin extends Entity {
         double maxSpeed = configFile.getMaxSpeed();
         double curveStart = configFile.getCurveStart();
         double curveEnd = configFile.getCurveEnd();
-        double additionalSpeed;
+        double modHSpeed;
 
-        if (configFile.isAltitudeDeterminesSpeed() && curveStart < playerAltitude)
-            additionalSpeed = EanMath.getLinealValue(curveStart,Math.sqrt(minSpeed),curveEnd,Math.sqrt(maxSpeed),playerAltitude);
-        else
-            additionalSpeed = minSpeed;
+        // * Clamp the calculated modified speed to not be below or over the speed range.
+        modHSpeed = (configFile.isAltitudeDeterminesSpeed()) ? MathHelper.clamp(EanMath.getLinealValue(curveStart,minSpeed,curveEnd,maxSpeed,playerAltitude), minSpeed, maxSpeed) : minSpeed;
 
         Vec3d movementVector = this.getVelocity();
         if (movementVector.y > -0.5) {
@@ -91,15 +89,17 @@ public abstract class LivingEntityMixin extends Entity {
         float fallSpeedMultiplier = MathHelper.cos(pitchInRadians);
         fallSpeedMultiplier = (float)((double)fallSpeedMultiplier * ((double)fallSpeedMultiplier * Math.min(1.0, rotationVectorLength / 0.4)));
 
-        movementVector = this.getVelocity().add(0.0, speedConstant * (-1.0 + (double)fallSpeedMultiplier * 0.75), 0.0);
+        movementVector = this.getVelocity().add(0.0, speedConstant * (-1.0 + (double)fallSpeedMultiplier * 0.75), 0.0); // ! Set Y=0.0 to turn off downwards speed.
 
         // * Looking under the horizon
         // Horizontal movement uses aux plus the (+1 m/s) constant multiplied by the speed set by the player minus default speed.
         if (movementVector.y < 0.0 && angleToTheGround > 0.0) {
             aux = movementVector.y * -0.1 * (double)fallSpeedMultiplier;
-            aux2 = aux + (additionalSpeed-30.35)*0.0005584565076792029D;
 
-            movementVector = movementVector.add(rotationVector.x * aux2 / angleToTheGround, aux, rotationVector.z * aux2 / angleToTheGround);
+            // ! The value 30.1298 should only be subtracted when downwards speed is active, since this speed affects the horizontal speed that it is already tweaked to almost perfectly reflect config file values.
+            aux2 = aux + (modHSpeed-30.1298D)*0.0005584565076792029D; // ? This is the 1 m/s constant, it is not 100% accurate, but it is close enough.
+
+            movementVector = movementVector.add(rotationVector.x * aux2 / angleToTheGround, aux, rotationVector.z * aux2 / angleToTheGround); // ! Set Y=0.0 to turn off downwards speed.
         }
 
         // * Looking over the horizon
