7c3796b60e5dee9bbdecee65ac04afb56be70764
Soulphur0 <ismaelbe2002@gmail.com>
2023-03-16 20:25:55 +0100

    On my way to properly instantiate everything configuration related if the dependency is present.

--- src/main/java/com/github/Soulphur0/ElytraAeronautics.java
+++ src/main/java/com/github/Soulphur0/ElytraAeronautics.java
@@ -1,11 +1,11 @@
 package com.github.Soulphur0;
 
 import com.github.Soulphur0.behaviour.EanCloudRenderBehaviour;
-import com.github.Soulphur0.behaviour.EanFlightBehaviour;
 import com.github.Soulphur0.config.EanCommands;
 import com.github.Soulphur0.config.EanConfig;
-import com.github.Soulphur0.config.singletons.CloudLayer;
-import com.github.Soulphur0.config.singletons.ElytraFlight;
+import com.github.Soulphur0.config.singletons.CloudConfig;
+import com.github.Soulphur0.config.singletons.FlightConfig;
+import com.github.Soulphur0.integration.DependencyChecker;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
@@ -19,16 +19,26 @@ public class ElytraAeronautics implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		AutoConfig.register(EanConfig.class, GsonConfigSerializer::new);
-		AutoConfig.getConfigHolder(EanConfig.class).registerSaveListener((configHolder, eanConfig)->{
-			CloudLayer.generateCloudLayers(eanConfig);
-			EanCloudRenderBehaviour.configUpdated = true;
-			return ActionResult.PASS;
-		});
-		AutoConfig.getConfigHolder(EanConfig.class).registerSaveListener(((configHolder, eanConfig) -> {
-			ElytraFlight.refresh(eanConfig);
-			return ActionResult.PASS;
-		}));
+		// ? Check for ClothConfig, if not present, create config file manually.
+		if (DependencyChecker.checkForClothConfig()){
+			// Register config class as config screen
+			AutoConfig.register(EanConfig.class, GsonConfigSerializer::new);
+
+			// Register flight config listener
+			AutoConfig.getConfigHolder(EanConfig.class).registerSaveListener(((configHolder, eanConfig) -> {
+				FlightConfig.readConfig(eanConfig);
+				return ActionResult.PASS;
+			}));
+
+			// Register cloud config listener
+			AutoConfig.getConfigHolder(EanConfig.class).registerSaveListener((configHolder, eanConfig)->{
+				CloudConfig.CloudLayer.readConfig(eanConfig);
+				EanCloudRenderBehaviour.configUpdated = true;
+				return ActionResult.PASS;
+			});
+		} else {
+
+		}
 		EanCommands.register();
 
 		LOGGER.info("Elytra Aeronautics initialized! Have a good flight!");
