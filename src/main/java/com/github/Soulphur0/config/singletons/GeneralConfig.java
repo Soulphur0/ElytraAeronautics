066b08fb6f5278eaca2c3e6685637f105183bd2c
Soulphur0 <ismaelbe2002@gmail.com>
2023-03-16 20:23:11 +0100

    This class will hold in the future the ClothConfig configuration instance, or its own instance if said library is not present. When saving data from FlightConfig or CloudConfig, this class will be the one that takes the attributes from both classes and stores them in a JSON config file, that is if ClothConfig is not present, again.

---
+++ src/main/java/com/github/Soulphur0/config/singletons/GeneralConfig.java
@@ -0,0 +1,11 @@
+package com.github.Soulphur0.config.singletons;
+
+import com.github.Soulphur0.config.EanConfig;
+
+public class GeneralConfig {
+    EanConfig eanConfig;
+
+    public static void write(){
+        if ()
+    }
+}
