188cbea2bb18df48f91e72b75da76f8e1cdb6414
Soulphur0 <ismaelbe2002@gmail.com>
2023-03-17 14:14:47 +0100

    Made CloudLayer an outer class; this will make things easier even if I will have to be managing more classes around.

--- src/main/java/com/github/Soulphur0/config/singletons/CloudConfig.java
+++ src/main/java/com/github/Soulphur0/config/singletons/CloudConfig.java
@@ -1,11 +1,11 @@
 package com.github.Soulphur0.config.singletons;
 
-import com.github.Soulphur0.config.EanConfig;
+import com.github.Soulphur0.config.clothConfig.CloudConfigScreen;
+import com.github.Soulphur0.config.objects.CloudLayer;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
-import net.minecraft.client.render.BufferBuilder;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -29,28 +29,33 @@ public class CloudConfig {
     @Expose
     private boolean useEanClouds;
     @Expose
-    protected CloudTypes cloudType;
+    private CloudTypes cloudType;
     @Expose
-    protected float verticalRenderDistance;
+    private float verticalRenderDistance;
     @Expose
-    protected int horizontalRenderDistance;
+    private int horizontalRenderDistance;
     @Expose
-    protected float lodRenderDistance;
+    private float lodRenderDistance;
 
     // ; Cloud style settings
     @Expose
-    protected float cloudSpeed;
+    private float cloudSpeed;
     @Expose
-    protected float cloudThickness;
+    private float cloudThickness;
     @Expose
-    protected int cloudColor;
+    private int cloudColor;
     @Expose
-    protected float cloudOpacity;
+    private float cloudOpacity;
     @Expose
-    protected boolean shading;
+    private boolean shading;
 
-    // = Config instance and layers loaded in memory.
+    // = Config instance
+    // ; The singleton instance stores and manages general cloud config data.
+    // ; Fields are made private to conditionally write the data directly into the config screen via its setters.
     public static CloudConfig instance;
+
+    // = Layers loaded in memory.
+    // ; The static array of cloud layers stores all cloud layers in an easier to access way.
     public static CloudLayer[] cloudLayers;
 
     // $ CLASS METHODS
@@ -61,11 +66,12 @@ public class CloudConfig {
         return instance;
     }
 
-    // ? If ClothConfig is installed, read the config screen; otherwise read the config file directly.
-    // ¿ The config file must exist first, for this reason write will always be called preemptively at mod initiation.
-    public void readConfig(EanConfig... optional){
+    // ? Method accessed via the ClothConfig save listener
+    // ¿ Updates the config instance with the config screen values.
+    public static void updateConfig(CloudConfigScreen... optional){
+        getOrCreateInstance();
         try {
-            EanConfig config = optional[0];
+            CloudConfigScreen config = optional[0];
 
             // Load general config settings to the singleton instance.
             instance.setNumberOfLayers(config.numberOfLayers);
@@ -246,176 +252,6 @@ public class CloudConfig {
 
     public void setShading(boolean shading) {
         this.shading = shading;
-    }
-
-    public class CloudLayer extends CloudConfig{
-        // ; Cloud positioning settings
-        @Expose
-        private double altitude;
-
-        // = Contextual attributes for rendering
-        private BufferBuilder.BuiltBuffer vertexGeometry; // The geometry of the processed cloud layer to later render.
-        private double renderAltitude; // The 'p' parameter of the original rendering code, it stores the exact render altitude. Which is the remainder of cloud render altitude when measured in cloud thickness.
-        private float displacement;
-        private boolean withinRenderDistance;
-        private boolean withinLodRenderDistance;
-        private float translationX;
-        private float translationZ;
-
-        // $ GETTERS & SETTERS
-        @Override
-        public CloudTypes getCloudType() {
-            return super.getCloudType();
-        }
-
-        @Override
-        public void setCloudType(CloudTypes cloudType) {
-            super.setCloudType(cloudType);
-        }
-
-        @Override
-        public float getVerticalRenderDistance() {
-            return super.getVerticalRenderDistance();
-        }
-
-        @Override
-        public void setVerticalRenderDistance(float verticalRenderDistance) {
-            super.setVerticalRenderDistance(verticalRenderDistance);
-        }
-
-        @Override
-        public int getHorizontalRenderDistance() {
-            return super.getHorizontalRenderDistance();
-        }
-
-        @Override
-        public void setHorizontalRenderDistance(int horizontalRenderDistance) {
-            super.setHorizontalRenderDistance(horizontalRenderDistance);
-        }
-
-        @Override
-        public float getLodRenderDistance() {
-            return super.getLodRenderDistance();
-        }
-
-        @Override
-        public void setLodRenderDistance(float lodRenderDistance) {
-            super.setLodRenderDistance(lodRenderDistance);
-        }
-
-        @Override
-        public float getCloudSpeed() {
-            return super.getCloudSpeed();
-        }
-
-        @Override
-        public void setCloudSpeed(float cloudSpeed) {
-            super.setCloudSpeed(cloudSpeed);
-        }
-
-        @Override
-        public float getCloudThickness() {
-            return super.getCloudThickness();
-        }
-
-        @Override
-        public void setCloudThickness(float cloudThickness) {
-            super.setCloudThickness(cloudThickness);
-        }
-
-        @Override
-        public int getCloudColor() {
-            return super.getCloudColor();
-        }
-
-        @Override
-        public void setCloudColor(int cloudColor) {
-            super.setCloudColor(cloudColor);
-        }
-
-        @Override
-        public float getCloudOpacity() {
-            return super.getCloudOpacity();
-        }
-
-        @Override
-        public void setCloudOpacity(float cloudOpacity) {
-            super.setCloudOpacity(cloudOpacity);
-        }
-
-        @Override
-        public boolean isShading() {
-            return super.isShading();
-        }
-
-        @Override
-        public void setShading(boolean shading) {
-            super.setShading(shading);
-        }
-
-        public double getAltitude() {
-            return altitude;
-        }
-
-        public void setAltitude(double altitude) {
-            this.altitude = altitude;
-        }
-
-        public BufferBuilder.BuiltBuffer getVertexGeometry() {
-            return vertexGeometry;
-        }
-
-        public void setVertexGeometry(BufferBuilder.BuiltBuffer vertexGeometry) {
-            this.vertexGeometry = vertexGeometry;
-        }
-
-        public double getRenderAltitude() {
-            return renderAltitude;
-        }
-
-        public void setRenderAltitude(double renderAltitude) {
-            this.renderAltitude = renderAltitude;
-        }
-
-        public float getDisplacement() {
-            return displacement;
-        }
-
-        public void setDisplacement(float displacement) {
-            this.displacement = displacement;
-        }
-
-        public boolean isWithinRenderDistance() {
-            return withinRenderDistance;
-        }
-
-        public void setWithinRenderDistance(boolean withinRenderDistance) {
-            this.withinRenderDistance = withinRenderDistance;
-        }
-
-        public boolean isWithinLodRenderDistance() {
-            return withinLodRenderDistance;
-        }
-
-        public void setWithinLodRenderDistance(boolean withinLodRenderDistance) {
-            this.withinLodRenderDistance = withinLodRenderDistance;
-        }
-
-        public float getTranslationX() {
-            return translationX;
-        }
-
-        public void setTranslationX(float translationX) {
-            this.translationX = translationX;
-        }
-
-        public float getTranslationZ() {
-            return translationZ;
-        }
-
-        public void setTranslationZ(float translationZ) {
-            this.translationZ = translationZ;
-        }
     }
 
     public enum CloudTypes {
