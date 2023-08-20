0e77fca8d280bd7909bb84cffa713d318b17f0af
Soulphur0 <ismaelbe2002@gmail.com>
2023-08-20 19:49:25 +0200

    Added explanations, refactored some names to better fit their variables' purposes.

--- src/main/java/com/github/Soulphur0/behaviour/EanFlightBehaviour.java
+++ src/main/java/com/github/Soulphur0/behaviour/EanFlightBehaviour.java
@@ -8,7 +8,7 @@ import net.minecraft.util.math.Vec3d;
 
 public class EanFlightBehaviour {
 
-    // : Cloud rendering entry point for injection/modification.
+    // : Flight entry point for injection/modification.
     static public Vec3d ean_flightBehaviour(LivingEntity player){
         return ean_setupFlightCalc(player);
     }
@@ -35,6 +35,10 @@ public class EanFlightBehaviour {
             }
         }
 
+        // = Return null vector if altitude-determined speed is disabled
+        if (!configInstance.isAltitudeDeterminesSpeed())
+            return null;
+
         // + Get player altitude
         Vec3d positionVector = player.getPos();
         double playerAltitude = positionVector.y;
@@ -46,9 +50,9 @@ public class EanFlightBehaviour {
     }
 
     private static Vec3d ean_calcFlightMovementVector(FlightConfig configInstance, LivingEntity player, double playerAltitude){
-        double speedConstant = 0.08;
-        double aux;
-        double aux2;
+        double fallSpeedConstant = 0.08;
+        double verticalSpeedValue;
+        double horizontalSpeedValue;
 
         // ? Read config file values
         double minSpeed = configInstance.getMinSpeed();
@@ -58,7 +62,7 @@ public class EanFlightBehaviour {
 
         // + Calculate additional speed based on player altitude.
         // * Clamp the calculated modified speed to not be below or over the speed range.
-        double modHSpeed = (configInstance.isAltitudeDeterminesSpeed()) ? MathHelper.clamp(EanMath.getLinealValue(curveStart,minSpeed,curveEnd,maxSpeed,playerAltitude), minSpeed, maxSpeed) : minSpeed;
+        double altitudeCalculatedSpeed = (configInstance.isAltitudeDeterminesSpeed()) ? MathHelper.clamp(EanMath.getLinealValue(curveStart,minSpeed,curveEnd,maxSpeed,playerAltitude), minSpeed, maxSpeed) : minSpeed;
 
         Vec3d movementVector = player.getVelocity();
         if (movementVector.y > -0.5) {
@@ -71,28 +75,42 @@ public class EanFlightBehaviour {
         double speed = movementVector.horizontalLength();
         double rotationVectorLength = rotationVector.length();
 
+        // $ Vertical speed calculations
+
+        // + Calculate the fall speed multiplier based on the player's flight pitch.
         float fallSpeedMultiplier = MathHelper.cos(pitchInRadians);
         fallSpeedMultiplier = (float)((double)fallSpeedMultiplier * ((double)fallSpeedMultiplier * Math.min(1.0, rotationVectorLength / 0.4)));
 
-        movementVector = player.getVelocity().add(0.0, speedConstant * (-1.0 + (double)fallSpeedMultiplier * 0.75), 0.0); // ! Set Y=0.0 to turn off downwards speed.
+        // + Make the player always lose altitude.
+        // * A greater flight pitch and therefore fallSpeedMultiplier, lead to a greater downwards vertical velocity.
+        // % Set Y=0.0 to turn off downwards speed.
+        movementVector = player.getVelocity().add(0.0, fallSpeedConstant * (-1.0 + (double)fallSpeedMultiplier * 0.75), 0.0);
+
+        // $ Horizontal speed and movement vector calculations
 
         // + Looking under the horizon
-        // * Horizontal movement uses aux plus the (+1 m/s) constant multiplied by the speed set by the player minus default speed.
+        // * Horizontal movement uses verticalSpeedValue plus the (+1 m/s) constant multiplied by the speed set by the player (minus default speed).
         if (movementVector.y < 0.0 && angleToTheGround > 0.0) {
-            aux = movementVector.y * -0.1 * (double)fallSpeedMultiplier;
+            verticalSpeedValue = movementVector.y * -0.1 * (double)fallSpeedMultiplier;
 
-            // ! The value 30.1298 should only be subtracted when downwards speed is active, since this speed affects the horizontal speed that it is already tweaked to almost perfectly reflect config file values.
-            aux2 = aux + (modHSpeed-30.1298D)*0.0005584565076792029D; // ? This is the 1 m/s constant, it is not 100% accurate, but it is close enough.
+            // ! Total Speed: The configured speed value minus the vanilla speed value.
+            // % The value 30.1298 should only be subtracted when downwards speed (fall speed) is active, since this speed affects the horizontal speed that it is already tweaked to almost perfectly reflect config file values.
+            double totalSpeed = (altitudeCalculatedSpeed-30.1298D);
 
-            movementVector = movementVector.add(rotationVector.x * aux2 / angleToTheGround, aux, rotationVector.z * aux2 / angleToTheGround); // ! Set Y=0.0 to turn off downwards speed.
+            // ? Horizontal speed value: vertical speed is added to the total speed multiplied by the 1 m/s constant.
+            // ¿ The 1 m/s constant is not 100% accurate, but it is close enough.
+            horizontalSpeedValue = verticalSpeedValue + totalSpeed*0.0005584565076792029D;
+
+            // % Set Y=0.0 to turn off downwards speed.
+            movementVector = movementVector.add(rotationVector.x * horizontalSpeedValue / angleToTheGround, verticalSpeedValue, rotationVector.z * horizontalSpeedValue / angleToTheGround);
         }
 
         // + Looking over the horizon
         // * Vertical speed decreases with the player realtime speed.
         if (pitchInRadians < 0.0f && angleToTheGround > 0.0) {
-            aux = speed * (double)(-MathHelper.sin(pitchInRadians)) * 0.04;
+            verticalSpeedValue = speed * (double)(-MathHelper.sin(pitchInRadians)) * 0.04;
 
-            movementVector = movementVector.add(-rotationVector.x * aux / angleToTheGround, Math.min((aux * 3.2), 0.1D), -rotationVector.z * aux / angleToTheGround);
+            movementVector = movementVector.add(-rotationVector.x * verticalSpeedValue / angleToTheGround, Math.min((verticalSpeedValue * 3.2), 0.1D), -rotationVector.z * verticalSpeedValue / angleToTheGround);
         }
 
         if (angleToTheGround > 0.0) {
